98feb0e2bcfa751e658d7ed370a41ee4b077ef8a
Mike Weaver <mweaver@invoca.com>
2020-04-25 15:44:53 -0700

    Improve test names for enumerable module

--- test/unit/enumerable_test.rb
+++ test/unit/enumerable_test.rb
@@ -7,7 +7,7 @@ require_relative '../test_helper'
 class EnumerableTest < Minitest::Test
 
   context 'build_hash' do
-    should 'convert arrays of two items to hash key/value pairs' do
+    should 'convert arrays of [key, value] to a hash of { key => value }' do
       assert_equal({ 'some' => 4, 'words' => 5 }, ['some', 'short', 'words'].build_hash { |s| [s, s.length] unless s == 'short' })
     end
 
@@ -17,15 +17,15 @@ class EnumerableTest < Minitest::Test
 
     # these seem like erroneous behavior, but, they have been left as-is for backward compatibility with hobosupport::Enumerable::build_hash
 
-    should 'convert arrays with one item by using the same value for key and value' do
+    should 'convert arrays of [single_value] to a hash of { single_value => single_value }' do
       assert_equal({ 'some' => 4, 'short' => 'short', 'words' => 5 }, ['some', 'short', 'words'].build_hash { |s| s == 'short' ? [s] : [s, s.length] })
     end
 
-    should 'convert arrays with more than two items by using the last item as the value' do
+    should 'convert arrays of [first, ..., last] to a hash of { first => last }' do
       assert_equal({ 'some' => 4, 'short' => 'three', 'words' => 5 }, ['some', 'short', 'words'].build_hash { |s| s == 'short' ? [s, 'two', 'three'] : [s, s.length] })
     end
 
-    should 'convert empty arrays into nil keys and values' do
+    should 'convert empty arrays to a hash of { nil => nil }' do
       assert_equal({ 'some' => 4, nil => nil, 'words' => 5 }, ['some', 'short', 'words'].build_hash { |s| s == 'short' ? [] : [s, s.length] })
     end
   end
