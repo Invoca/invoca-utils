137b00bf3185592776bb3a6b41b453a13cbc2158
Colin Kelley <colindkelley@gmail.com>
2020-06-06 15:58:12 -0700

    no-jira: omit redundant begin/end; update inline rdocs

--- lib/invoca/utils/exceptions.rb
+++ lib/invoca/utils/exceptions.rb
@@ -3,16 +3,22 @@
 module Invoca
   module Utils
     class << self
+      # Yields and rescues any exceptions given in `exception_classes`, retrying the given number of times.
+      # The final retry does not rescue any exceptions.
+      # The try number (0..retries) is yielded as a block param.
+      #
+      # @param [Class or Array(Class)] exception_classes - exception()s) to rescue
+      # @param [Integer] retries: - 1+ count of retries (1 retry = up to 2 tries total)
+      # @param [Proc] before_retry - optional proc which is called before each retry, with the exception passed as a block param
+      # @return the value from yield
       def retry_on_exception(exception_classes, retries:, before_retry: nil)
         retries.times do |attempt_number|
-          begin
-            return yield(attempt_number)
-          rescue *Array(exception_classes) => ex
-            before_retry&.call(ex)
-          end
+          return yield(attempt_number)
+        rescue *Array(exception_classes) => ex
+          before_retry&.call(ex)
         end
 
-        yield(retries)   # no rescue for this last time, so any exceptions will raise out
+        yield(retries)   # no rescue for this last try, so any exceptions will raise out
       end
     end
   end
