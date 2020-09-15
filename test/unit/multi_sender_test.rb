9b5ddd9a6e1421fa2030906dc913f836cd93a7fd
Yinon Rousso <yrousso@invoca.com>
2020-09-15 08:19:24 -0700

    TECH-4828 change multi_sender_test to use rspec

--- test/unit/multi_sender_test.rb
+++ test/unit/multi_sender_test.rb
@@ -3,7 +3,7 @@
 require_relative '../../lib/invoca/utils/multi_sender.rb'
 require_relative '../test_helper'
 
-class MultiSenderTest < Minitest::Test
+describe Invoca::Utils::MultiSender do
   # create enumerable class for testing
   class LinkedList
     include Enumerable
@@ -30,27 +30,25 @@ class MultiSenderTest < Minitest::Test
     end
   end
 
-  context 'MultiSender' do
-    context 'with custom Enumerable' do
-      setup do
-        linked_list = LinkedList.new('some') << 'short' << 'words'
-        @multi_sender = Invoca::Utils::MultiSender.new(linked_list, :map)
-      end
-
-      should 'call the same method on each item in an Enumerable and return the results as an array' do
-        assert_equal([5, 5, 4], @multi_sender.length)
-      end
-
-      should 'handle methods with arguments' do
-        assert_equal(['or', 'ho', 'om'], @multi_sender.slice(1, 2))
-      end
+  context 'with custom Enumerable' do
+    before do
+      linked_list = LinkedList.new('some') << 'short' << 'words'
+      @multi_sender = Invoca::Utils::MultiSender.new(linked_list, :map)
     end
 
-    context 'with built-in Array' do
-      should 'call the same method on each item in an Array and return the results as an array' do
-        multi_sender = Invoca::Utils::MultiSender.new(['some', 'short', 'words'], :map)
-        assert_equal([4, 5, 5], multi_sender.length)
-      end
+    it 'call the same method on each item in an Enumerable and return the results as an array' do
+      expect(@multi_sender.length).to eq([5, 5, 4])
+    end
+
+    it 'handle methods with arguments' do
+      expect(@multi_sender.slice(1, 2)).to eq(['or', 'ho', 'om'])
+    end
+  end
+
+  context 'with built-in Array' do
+    it 'call the same method on each item in an Array and return the results as an array' do
+      multi_sender = Invoca::Utils::MultiSender.new(['some', 'short', 'words'], :map)
+      expect(multi_sender.length).to eq([4, 5, 5])
     end
   end
 end
