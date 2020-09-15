53229d63c9741250fe9f412471c5e50ca596e26f
Yinon Rousso <yrousso@invoca.com>
2020-09-15 08:12:20 -0700

    TECH-4828 change map_compact_test to use rspec

--- test/unit/map_compact_test.rb
+++ test/unit/map_compact_test.rb
@@ -2,24 +2,24 @@
 
 require_relative '../test_helper'
 
-class MapCompactTest < Minitest::Test
-  should "map_compact" do
-    assert_equal [1, 9], [1, 2, nil, 3, 4].map_compact { |item| item**2 if (nil == item ? nil : item.odd?) }
+describe Enumerable do
+  it "map_compact" do
+    expect([1, 2, nil, 3, 4].map_compact { |item| item**2 if (nil == item ? nil : item.odd?) }).to eq([1, 9])
   end
 
-  should "map_compact to empty if nothing matches" do
-    assert_equal [], {:a => 'aaa', :b => 'bbb'}.map_compact { |key, value| value if key == :c }
+  it "map_compact to empty if nothing matches" do
+    expect({:a => 'aaa', :b => 'bbb'}.map_compact { |key, value| value if key == :c }).to eq([])
   end
 
-  should "map_compact a hash" do
-    assert_equal ['bbb'], {:a => 'aaa', :b => 'bbb'}.map_compact { |key, value| value if key == :b }
+  it "map_compact a hash" do
+    expect({:a => 'aaa', :b => 'bbb'}.map_compact { |key, value| value if key == :b }).to eq(['bbb'])
   end
 
-  should "map_compact empty collection" do
-    assert_equal [], [].map_compact { |item| true }
+  it "map_compact empty collection" do
+    expect([].map_compact { |item| true }).to eq([])
   end
 
-  should "not map_compact false" do
-    assert_equal [false], [nil, false].map_compact { |a| a }
+  it "not map_compact false" do
+    expect([nil, false].map_compact { |a| a }).to eq([false])
   end
 end
