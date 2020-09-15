bb89e9cb9f53a0e43b4779b00f8b196b789cf086
Yinon Rousso <yrousso@invoca.com>
2020-09-15 11:02:25 -0700

    TECH-4828 update test formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
-require "minitest/autorun"
 require 'rr'
 require 'shoulda'
 require 'pry'
 require 'active_support/all'
 
 require 'invoca/utils'
-require "minitest/reporters"
-Minitest::Reporters.use! [
-  Minitest::Reporters::DefaultReporter.new,
-  Minitest::Reporters::JUnitReporter.new
-]
+require 'rspec_junit_formatter'
+
+RSpec.configure do |config|
+  config.add_formatter(RspecJunitFormatter, 'spec/reports/rspec.xml')
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  RSpec::Support::ObjectFormatter.default_instance.max_formatted_output_length = 2_000
+end
